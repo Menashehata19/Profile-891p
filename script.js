50b6b04de39b7618d2a4a76aff11cd3295920bd7
Menashehata19 <161930847+Menashehata19@users.noreply.github.com>
2024-05-09 15:18:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ if (localStorage.getItem('theme') === 'dark') {
 
 // تبديل الوضع المظلم وحفظ التفضيل في localStorage
 function toggleDarkMode() {
-  divmen.style.display="none"
+  
   var element = document.body;
   element.classList.toggle('dark-mode');
   
